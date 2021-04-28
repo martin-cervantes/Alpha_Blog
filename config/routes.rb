32a7b6100c8c26cd50210823b38617e6a77c76af
mcervantes71 <cervantes.martine@gmail.com>
2021-04-28 15:51:13 -0600

    Add user authetication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'signup', to: 'users#new'
+  get    'login',  to: 'sessions#new'
+  post   'login',  to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get    'signup', to: 'users#new'
   resources :users, except: [:new]
-  
+
   root to: "pages#index"
 
   resources :articles
