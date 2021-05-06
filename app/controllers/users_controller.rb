44791f81f563ef1f96ea815f990491e7b2720572
mcervantes71 <cervantes.martine@gmail.com>
2021-05-06 13:35:26 -0600

    Check if admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,7 +60,7 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      session[:user_id] = nil
+      session[:user_id] = nil if @user == current_user
       format.html { redirect_to users_url, notice: 'Account and all associated articles successfully deleted.' }
       format.json { head :no_content }
     end
@@ -78,8 +78,8 @@ class UsersController < ApplicationController
     end
 
     def require_same_user
-      if current_user != @user
-        flash[:alert] = "You can only edit your own account"
+      if current_user != @user && !current_user.admin?
+        flash[:alert] = "You can only edit or delete your own account"
         redirect_to @user
       end
     end
