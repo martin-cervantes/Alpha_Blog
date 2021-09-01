46f66c492889ae322a5333fc122a3777ba167b78
Martin Cervantes <cervantes.martine@gmail.com>
2021-08-31 20:23:28 -0600

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+u = User.new
+u.username = "mcervantes"
+u.email = "mcervantes@hotcakes.com"
+u.password = "123456"
+u.save
